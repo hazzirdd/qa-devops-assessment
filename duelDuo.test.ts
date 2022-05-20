37a4841c3e8c409221fd1a02926df9ab508174f6
Hayden Sweet <haydensweet22@gmail.com>
2022-05-20 16:51:00 -0600

    review

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -9,22 +9,22 @@ beforeEach(async () => {
     driver.get('http://localhost:3000/')
 })
 
-// afterAll(async () => {
-//     driver.quit()
-// })
+afterAll(async () => {
+    driver.quit()
+})
 
 test('Title shows up when page loads', async () => {
     const title = await driver.findElement(By.id('title'))
     const displayed = await title.isDisplayed()
     expect(displayed).toBe(true)
-    await driver.sleep(2000)
+    await driver.sleep(1000)
 })
 
 test('I can draw 5 robot cards', async () => {
     await driver.findElement(By.id('draw')).click()
     await driver.sleep(1000)
-    let cards = await driver.findElement(By.xpath('//div/div[@class="bot-card outline"]'))
-    const displayed = cards.isDisplayed()
+    let cards = await driver.findElement(By.xpath('(//div/div[contains(@class, "bot-card")])[1]'))
+    const displayed = await cards.isDisplayed()
     expect(displayed).toBe(true)
   
     await driver.sleep(1000)
--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/api/robots', (req, res) => {
     try {
-        res.status(200).send(botsArr)
+        res.status(200).send(bots)
     } catch (error) {
         console.log('ERROR GETTING BOTS', error)
         res.sendStatus(400)
